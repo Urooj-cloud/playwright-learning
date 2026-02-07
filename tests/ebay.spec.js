7733b899c2f77a6d6fc5731cfa29c0cb22ada53a
Urooj-cloud <uroojhaq9090@gmail.com>
2026-02-07 11:07:57 +0500

    Fixed eBay test: Added price extraction and cart assertion

--- tests/ebay.spec.js
+++ tests/ebay.spec.js
@@ -2,12 +2,13 @@ const {test,expect}=require('@playwright/test');
 
 test('test', async ({ page }) => {
   await page.goto('https://www.ebay.com/');
-  await page.getByRole('combobox', { name: 'Search for anything' }).click();
-  await page.getByRole('combobox', { name: 'Search for anything' }).fill('iphone 13');
-  await page.locator('#ebay-ac-suggestion-4').getByText('13').click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.locator('#item5531195327').getByRole('link', { name: 'Apple iPhone 13 A2482 128 GB Midnight TracFone Very Good', exact: true }).click();
-  const page1 = await page1Promise;
-  await page1.getByTestId('x-atc-action').getByTestId('ux-call-to-action').click();
-  await page1.getByRole('button', { name: 'Close dialog' }).click();
+  await page.getByPlaceholder('Search for anything',{exact:false}).fill('Apple iphone 13 128GB-6.1');
+  await page.keyboard.press('Enter');
+  const pagePromise=page.waitForEvent('popup');
+  await page.getByAltText('Apple iphone 13 128GB Unlocked').first().click();
+  const newTab=await pagePromise;
+  await page.waitForLoadState();
+  await newTab.getByRole('button',{name:'Add to cart'}).click();
+  await expect(newTab.getByText('Added to cart')).toBeVisible();
+  console.log('iphone added to cart successfuly!');
 });
